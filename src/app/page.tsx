34df36000c8e2dfa3d4cead141842b136dc65d39
Eduardo Moraton <moraton.eduardo@gmail.com>
2024-01-20 16:03:41 +0100

    visual update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,18 +78,18 @@ export default function Home() {
   }
 
   return (
-    <main className='flex items-center justify-center flex-col w-screen h-screen bg-red-100 text-black'>
+    <main className='flex items-center justify-center flex-col w-screen h-screen bg-gray-100 text-black'>
       <div>
         <h1 className='text-5xl'>Alicante bus 🚌</h1>
       </div>
       <div className={'flex-grow flex justify-start flex-col'}>
         {stops.map((stop, index) => (
-          <div key={index} className={'w-60 bg-red-200 p-5 m-2 shadow-lg rounded-lg'}>
+          <div key={index} className={'w-[100%] nm-flat-white-lg s p-5 m-2 rounded-lg'}>
             <p>{stop.name}</p>
             <p>{"" + stop.code}</p>
-            <div className={'flex flex-col'}>
+            <div className={'flex flex-col gap-2'}>
               {stop.nextBuses.map((bus, busIndex) => (
-                <div key={busIndex} className={'flex'}>
+                <div key={busIndex} className={'flex nm-inset-gray-50-sm rounded-md p-2 gap-4'}>
                   <p className={'text-red-700 mr-2'}>{bus.kind}</p>
                   <p>{bus.direction}</p>
                   <p>{"" + bus.min}min</p>
@@ -99,16 +99,22 @@ export default function Home() {
           </div>
         ))}
       </div>
-      <div className={'bg-red-300 p-5 flex flex-col rounded-md m-2 shadow-lg'}>
-        <input
-          type='text'
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
+      <div className={'nm-flat-white-lg p-5 flex flex-col rounded-md m-2 shadow-lg'}>
+        <p>Código parada:</p>
+        <div className='nm-inset-white p-2 my-2 rounded'>
+          
+          <input
+            className=''
+            type='text'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </div>
         <div className={'flex text-2xl justify-evenly'}>
-          <button onClick={handleAdd}>➕</button>
-          <button onClick={handleSave}>💾</button>
-          <button onClick={handleUpdate}>🔄</button>
+          
+          <button className='p-2 rounded-full nm-convex-white active:nm-inset-white' onClick={handleAdd}>➕</button>
+          <button className='p-2 rounded-full nm-convex-white active:nm-inset-white' onClick={handleSave}>💾</button>
+          <button className='p-2 rounded-full nm-convex-white active:nm-inset-white' onClick={handleUpdate}>🔄</button>
         </div>
         
       </div>
